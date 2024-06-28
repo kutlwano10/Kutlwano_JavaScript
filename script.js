ef686977e78eee36ee4d6608e70c6ebceb747100
kutlwano Ramotebele <111123714+kutlwano10@users.noreply.github.com>
2024-06-28 14:32:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,41 +34,17 @@ let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 
-// Challenge 2(11.3: Asynchronous Programming, Building Promise.all)
-function Promise_all(promises) {
-  return new Promise((resolve, reject) => {
-    // Solution
-    let results = [];
-    let pending = promises.length;
-    for (let i = 0; i < promises.length; i++) {
-      promises[i].then(result => {
-        results[i] = result;
-        pending--;
-        if (pending == 0) resolve(results);
-      }).catch(reject);
-    }
-    if (promises.length == 0) resolve(results);
-  });
+// Challenge 2(11.function Recursion)
+function isEven(n) {
+  if (n == 0) return true;
+  else if (n == 1) return false;
+  else if (n < 0) return isEven(-n);
+  else return isEven(n - 2);
 }
 
-// Test code.
-Promise_all([]).then(array => {
-  console.log("This should be []:", array);
-});
-function soon(val) {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(val), Math.random() * 500);
-  });
-}
-Promise_all([soon(1), soon(2), soon(3)]).then(array => {
-  console.log("This should be [1, 2, 3]:", array);
-});
-Promise_all([soon(1), Promise.reject("X"), soon(3)])
-  .then(array => {
-    console.log("We should not get here");
-  })
-  .catch(error => {
-    if (error != "X") {
-      console.log("Unexpected failure:", error);
-    }
-  });
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → false
